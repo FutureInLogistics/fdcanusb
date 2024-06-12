6bb43ea587311ceb182cadeb12d931c5d53f8b92
Igor Knippenberg <igor.knippenberg@gmail.com>
2024-06-12 17:24:40 +0200

    Change settings for fdcanusb_drive

--- fw/can_manager.cc
+++ fw/can_manager.cc
@@ -33,13 +33,13 @@ constexpr std::size_t kFilterSize = 16;
 
 struct Config {
   int32_t bitrate = 1000000;
-  int32_t fd_bitrate = 5000000;
+  int32_t fd_bitrate = 1000000;
   bool automatic_retransmission = false;
   bool fdcan_frame = true;
-  bool bitrate_switch = true;
+  bool bitrate_switch = false;
   bool restricted_mode = false;
   bool bus_monitor = false;
-  bool termination = true;
+  bool termination = false;
   bool autostart = true;
 
   // We by default start out set for a TCAN1057 with its longer loop delay:
